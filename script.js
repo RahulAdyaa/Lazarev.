9094ac9d2812a0dc5bb272bcc0423069bd06ccb0
Rahul adya <radya_be22@thapar.edu>
2024-10-14 00:36:41 +0530

    removed folder named LAZAREV

---
+++ script.js
@@ -0,0 +1,102 @@
+
+
+function navamination() {
+  var nav = document.querySelector("nav");
+  nav.addEventListener("mouseenter", function () {
+    let tl = gsap.timeline();
+    tl.to("#nav-bottom", {
+      height: "21vh",
+    });
+    tl.to(".nav-part2 h5", {
+      display: "block",
+    });
+    tl.to(".nav-part2 h5 span", {
+      y: 0,
+      stagger: {
+        amount: 0.8,
+      },
+    });
+  });
+  nav.addEventListener("mouseleave", function () {
+    let tl = gsap.timeline();
+
+    tl.to(".nav-part2 h5 span", {
+      y: 25,
+      stagger: {
+        amount: 0.2,
+      },
+    });
+    tl.to(".nav-part2 h5", {
+      display: "none",
+      duration: 0.1,
+    });
+    tl.to("#nav-bottom", {
+      height: 0,
+      duration: 0.2,
+    });
+  });
+}
+
+// navamination();
+
+
+//animation for moving image over elements
+function page2animation(){
+  
+var rightElems=document.querySelectorAll(".right-elem")
+
+
+rightElems.forEach(function(elem){
+  elem.addEventListener("mouseenter",function(){
+
+    console.log(elem.getBoundingClientRect())//give all info about that particular div
+    gsap.to(elem.childNodes[3],{
+      opacity:1,
+      scale:1
+    })
+  })
+  elem.addEventListener("mouseleave",function(){
+    gsap.to(elem.childNodes[3],{
+      opacity:0,
+      scale:0
+    })
+  })
+  elem.addEventListener("mousemove",function(dets){
+    gsap.to(elem.childNodes[3],{
+      x:dets.x-elem.getBoundingClientRect().x-90,
+      y:dets.y-elem.getBoundingClientRect().y-215
+    })
+  })
+})
+}
+
+page2animation()
+
+function page3VideoAnimation(){
+  
+var page3Center=document.querySelector(".page3-center")
+var video=document.querySelector("#page3 video")
+
+page3Center.addEventListener("click",function(){
+  video.play()
+  gsap.to(video,{
+    transform:"scaleX(1) scaleY(1)",
+    opacity:1,
+    borderRadius:0,
+
+  })
+})
+
+
+video.addEventListener("click",function(){
+  video.pause()
+  gsap.to(video,{
+    transform:"scaleX(0.7) scaleY(0)",
+    opacity:0,
+    borderRadius:"30px"
+
+  })
+})
+}
+
+page3VideoAnimation()
